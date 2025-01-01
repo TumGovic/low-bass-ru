79edf599ecd64145e71a184d69354133ff56e9c5
TumGovic1 <102865941+TumGovic@users.noreply.github.com>
2025-01-01 23:05:24 +0300

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+// pages/_app.tsx
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { NextUIProvider } from "@nextui-org/system";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <ThemeProvider>
+            <NextUIProvider>
+                    <Component {...pageProps} />
+            </NextUIProvider>
+        </ThemeProvider>
+    );
 }
